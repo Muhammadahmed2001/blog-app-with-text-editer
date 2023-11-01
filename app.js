ffc4dd3d00c3b5f6aba42012c52ce1bbd2fbb1fa
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-11-02 01:41:16 +0500

    add more option in tool bar

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+var toolbarOptions = [
+  ["bold", "italic", "underline", "strike"],
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ direction: "rtl" }],
+  [{ size: ["small", false, "large", "huge"] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }],
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"],
+];
 var quill = new Quill("#editor", {
   theme: "snow",
+  placeholder: "Write Blog...",
+  modules: {
+    toolbar: toolbarOptions,
+  },
 });
-
 
 var main = document.getElementById("main");
 
-
-function post(){
-  var editor_content = quill.root.innerHTML
-    main.innerHTML = `
+function post() {
+  var editor_content = quill.root.innerHTML;
+  main.innerHTML =
+    `
         <div class="col">
     <div class="card h-100">
         <div class="card-body">
@@ -17,8 +38,6 @@ function post(){
             </div>
         </div>
     </div>
-</div>` + main.innerHTML
-    quill.root.innerHTML = ""
-    
-    
+</div>` + main.innerHTML;
+  quill.root.innerHTML = "";
 }
